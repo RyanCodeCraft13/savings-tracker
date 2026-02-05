cd0cd7993cb1c60d65add47a4cd29b7ce995c83a
Ryan Christian Noble <154901707+RyanCodeCraft13@users.noreply.github.com>
2026-02-05 14:39:21 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,2 @@
-self.addEventListener("install",e=>{
-console.log("App Installed");
-});
-
-self.addEventListener("fetch",e=>{
-e.respondWith(fetch(e.request));
-});
+self.addEventListener("install",e=>console.log("App Installed"));
+self.addEventListener("fetch",e=>{e.respondWith(fetch(e.request));});
