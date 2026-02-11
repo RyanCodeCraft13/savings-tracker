d304c6a648b3329fe85e1e725d73c656b8a285eb
Ryan Christian Noble <154901707+RyanCodeCraft13@users.noreply.github.com>
2026-02-11 14:00:02 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,4 @@
-self.addEventListener("install",e=>self.skipWaiting());
-self.addEventListener("fetch",e=>{});
+const CACHE_NAME = "bank-tracker-cache-v1";
+const urlsToCache = ["./","./index.html","./manifest.json"];
+self.addEventListener("install", event => {event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));});
+self.addEventListener("fetch", event => {event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));});
